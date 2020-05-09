03c2a9c09b8181308536714f7dde5a590e276ed9
borelanjo <borelanjo@gmail.com>
2020-05-09 19:27:49 -0300

    Adiciona serviço para cerveja

---
+++ src/services/beer-service.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default class BeerService {
+  api = axios.create({
+    baseURL: `https://api.punkapi.com/v2/beers`
+  });
+
+  findAll() {
+    return this.api.get("");
+  }
+
+
+}
