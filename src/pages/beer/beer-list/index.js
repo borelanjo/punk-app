1e900132c3af13b753408d750df6924052a6d1cb
borelanjo <borelanjo@gmail.com>
2020-05-09 20:33:07 -0300

    Adiciona cards a tela de Listagem de Cerveja

--- src/pages/beer/beer-list/index.js
+++ src/pages/beer/beer-list/index.js
@@ -1,26 +1,59 @@
 import React from 'react';
 
+import './beer-list.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import BeerService from '../../../services/beer-service';
+
+import Load from '../../../components/load';
+import Header from '../../../components/header';
+import Button from '../../../components/button';
+import { Card, CardContent } from '../../../components/cards';
 
 export default class BeerList extends React.Component {
   service = new BeerService();
   state = {
-    beers: []
+    beers: [],
+    isLoad: false
   }
 
   componentDidMount() {
     this.service.findAll()
       .then(res => {
         const beers = res.data;
-        this.setState({ beers });
+        this.setState({...this.state, 
+             beers,
+             isLoad: true});
       })
   }
 
   render() {
-    return (
+    return  <>
+    {!this.state.isLoad ? (
+      <Load />
+    ) :(
+        <>
+      <Header>
+      </Header>
+      <div>
+      { this.state.beers.map(beer =>       
+      <Card>
+        <CardContent>
+            <ul className="beer-item">
+                <li><img src={beer.image_url} alt={beer.name}/></li>
+                <li>{beer.name}</li>
+                <li>{beer.tagline}</li>
+            </ul>
+            </CardContent>
+      </Card>)}
+      </div>
+
       <ul>
-        { this.state.beers.map(beer => <li>{beer.name}</li>)}
+        
       </ul>
+      </>
     )
+  }
+  </>
   }
 }
